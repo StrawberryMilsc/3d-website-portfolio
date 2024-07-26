2ca99d8e808841124ee85494c90536c5591d8397
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2024-07-25 21:49:33 -0700

    Updates

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
+const loader = new GLTFLoader();
 
-const sqr = new THREE.BoxGeometry(1,1,1)
-const sqr_mat = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
+loader.load(
+    '../models/trial.glb',  
+    function(gltf) {
+        scene.add(gltf.scene)
+    },
+    function(xhr) {
+        console.log((xhr.loaded/xhr.total * 100) + '% loaded');
+    },
+    function(error){
+        console.log("An error has occured")
+    }
+)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
 })
-
-const sqr_mesh = new THREE.Mesh(sqr,sqr_mat)
-scene.add(sqr_mesh)
-
-
-
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-camera.position.set(0,1,2)
+camera.position.set(0,3,7)
 scene.add(camera)
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+const sun = new THREE.DirectionalLight(0xffffff,1)
+sun.position.set(0,1,100)
+scene.add(sun)
+
+
+
+var controls = new OrbitControls( camera, renderer.domElement );
+    controls.enableDamping = true;
+
+
+
+
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
@@ -32,6 +48,7 @@ renderer.shadowMap.enabled = true
 
 function animate(){
     requestAnimationFrame(animate)
+    controls.update( );
     renderer.render(scene,camera)
 }
 
