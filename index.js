a6a2ab39cd9933b3b5f588e975542be8e9b291e8
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2024-08-06 02:57:12 -0700

    Updates

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-const canvas = document.querySelector('.webgl')
+
+import trial_v from './shaders/trial_v.glsl.js'
+import trial_f from './shaders/trial_f.glsl.js'
+
+
+const canvas = document.querySelector('.webgl2')
 const scene = new THREE.Scene()
 const loader = new GLTFLoader();
-
-loader.load(
-    'models/trial.glb',  
-    function(gltf) {
-        scene.add(gltf.scene)
-    },
-    function(xhr) {
-        console.log((xhr.loaded/xhr.total * 100) + '% loaded');
-    },
-    function(error){
-        console.log("An error has occured")
-    }
-)
+// LOading existing Modules into three js
+// loader.load(
+//     'models/trial.glb',  
+//     function(gltf) {
+//         scene.add(gltf.scene)
+//     },
+//     function(xhr) {
+//         console.log((xhr.loaded/xhr.total * 100) + '% loaded');
+//     },
+//     function(error){
+//         console.log("An error has occured")
+//     }
+// )
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: window.innerWidth/2,
+    height: window.innerHeight/2
 }
+const screenSize = new THREE.Vector2(sizes.width,sizes.height);
+const ico_geo = new THREE.SphereGeometry(5,32,32)
+const ico_mat = new THREE.ShaderMaterial({
+    uniforms: {
+        time: {value:1.0},
+        screenSize: {
+            value:screenSize
+        },
+        rad_js: {value: 5}
+    },
+
+    vertexShader: trial_v,
+    fragmentShader: trial_f
+});
+const ico = new THREE.Mesh(ico_geo,ico_mat)
+scene.add(ico);
+
+
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-camera.position.set(0,3,7)
+camera.position.set(0,-10,10)
 scene.add(camera)
-
-const sun = new THREE.DirectionalLight(0xffffff,1)
-sun.position.set(0,1,100)
-scene.add(sun)
-
 
 
 var controls = new OrbitControls( camera, renderer.domElement );
---
+++ shaders/trial_f.glsl.js
@@ -0,0 +1,6 @@
+export default `
+uniform vec2 screenSize;
+void main() {
+    vec3 color = vec3(1.0,1.0,1.0);
+    gl_FragColor = vec4(color,1.0);
+}`;
