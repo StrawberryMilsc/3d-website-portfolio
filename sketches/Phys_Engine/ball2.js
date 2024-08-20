f8f0920d3009bc423efc88011f6efe543494f079
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2024-08-19 18:46:33 -0700

    Updates

--- sketches/Phys_Engine/ball2.js
+++ sketches/Phys_Engine/ball2.js
@@ -73,25 +73,53 @@ var s = function( sketch ) {
     var yAcc = 1;
     const max_speed = 20;
     let butt;
-
+    sketch.windowResized = function() { 
+        w_n = window.innerWidth
+        sketch.resizeCanvas(w_n/2.5,w_n/2.5)
+        xPos = sketch.width/2;
+        yPos = sketch.height/4;
+        sketch.drawElements()
+    }
     sketch.setup = function() {
-        h = sketch.windowHeight;
-        w = sketch.windowWidth;
-        cnv = sketch.createCanvas(400,400 );
+        h = window.innerHeight;
+        w = window.innerWidth;
+        
+        sketch.cnv = sketch.createCanvas((w/2.5),w/2.5 );
+        scaleF = 200/w
+        xPos = sketch.width/2;
+        yPos = sketch.height/4;
         xPos = sketch.width/2;
         yPos = sketch.height/4;
         //cnv.parent("p5sketch2")
         sketch.background(255, 0, 200);
     }
-
+    sketch.drawElements = function() {
+        sketch.background(0);
+        
+        if (window.innerWidth > 900) {
+            sketch.fill(0,0,255,255)
+            sketch.circle(xPos,yPos,70);
+            sketch.fill(100,0,100,255)
+            sketch.rect(0,sketch.height-50,sketch.width,50)
+        } else if (window.innerWidth > 500) {
+            sketch.fill(0,0,255,255)
+            sketch.circle(xPos,yPos,50)
+            sketch.fill(100,0,100,255)
+            sketch.rect(0,sketch.height-30,sketch.width,50)
+        }   
+        else {
+            sketch.fill(0,0,255,255)
+            sketch.circle(xPos,yPos,30);
+            sketch.fill(100,0,100,255)
+            sketch.rect(0,sketch.height-10,sketch.width,50)
+        }
+        
+        
+    }
 
     //Fix Colors
     sketch.draw = function() {
-        sketch.background(0);
-        sketch.fill(0,0,255,255)
-        sketch.circle(xPos,yPos,70);
-        sketch.fill(100,0,100,255)
-        sketch.rect(0,sketch.height-50,sketch.width,50)
+        sketch.drawElements()
         sketch.update()
     }
     sketch.reset = function() {
@@ -135,13 +163,19 @@ var s = function( sketch ) {
     const rectHeight = 50;
     const max_speed = 10;
 
-
+    sketch.windowResized = function() { 
+        w_n = window.innerWidth
+        sketch.resizeCanvas(w_n/2.5,w_n/2.5)
+        xPos = sketch.width/2;
+        yPos = sketch.height/4;
+        sketch.drawElements()
+    }
     sketch.setup = function() {
-        h = sketch.windowHeight;
-        w = sketch.windowWidth;
-
-
-        cnv = sketch.createCanvas(400,400 );
+        h = window.innerHeight;
+        w = window.innerWidth;
+        
+        sketch.cnv = sketch.createCanvas((w/2.5),w/2.5 );
+        scaleF = 200/w
         xPos = sketch.width/2;
         yPos = sketch.height/4;
         rectPosY = sketch.height-50;
