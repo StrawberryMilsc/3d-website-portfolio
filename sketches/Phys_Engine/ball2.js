6c31d6e761dcbf2d868a37de3ef4a455d0af8aad
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2024-08-19 18:35:41 -0700

    Updates

--- sketches/Phys_Engine/ball2.js
+++ sketches/Phys_Engine/ball2.js
@@ -4,28 +4,54 @@ var s = function( sketch ) {
     var h;
     var xPos;
     var yPos;
-
-
-    sketch.setup = function() {
-        h = sketch.windowHeight;
-        w = sketch.windowWidth;
-
-
-        cnv = sketch.createCanvas(400,400 );
+    var scaleF;
+    sketch.windowResized = function() { 
+        w_n = window.innerWidth
+        sketch.resizeCanvas(w_n/2.5,w_n/2.5)
         xPos = sketch.width/2;
         yPos = sketch.height/4;
-        //cnv.parent("p5sketch")
-        sketch.background(255, 0, 200);
+        sketch.drawElements()
+    }
+
+    sketch.setup = function() {
+        h = window.innerHeight;
+        w = window.innerWidth;
+        
+        sketch.cnv = sketch.createCanvas((w/2.5),w/2.5 );
+        scaleF = 200/w
+        xPos = sketch.width/2;
+        yPos = sketch.height/4;
+        //sketch.cnv.parent("p5sketch")
+    }
+
+    sketch.drawElements = function() {
+        sketch.background(0);
+        
+        if (window.innerWidth > 900) {
+            sketch.fill(0,0,255,255)
+            sketch.circle(xPos,yPos,70);
+            sketch.fill(100,0,100,255)
+            sketch.rect(0,sketch.height-50,sketch.width,50)
+        } else if (window.innerWidth > 500) {
+            sketch.fill(0,0,255,255)
+            sketch.circle(xPos,yPos,50)
+            sketch.fill(100,0,100,255)
+            sketch.rect(0,sketch.height-30,sketch.width,50)
+        }   
+        else {
+            sketch.fill(0,0,255,255)
+            sketch.circle(xPos,yPos,30);
+            sketch.fill(100,0,100,255)
+            sketch.rect(0,sketch.height-10,sketch.width,50)
+        }
+        
+        
     }
 
 
     //Fix Colors
     sketch.draw = function() {
-        sketch.background(0);
-        sketch.fill(0,0,255,255)
-        sketch.circle(xPos,yPos,70);
-        sketch.fill(100,0,100,255)
-        sketch.rect(0,sketch.height-50,sketch.width,50)
+        sketch.drawElements();
     }
 
 
@@ -45,18 +71,16 @@ var s = function( sketch ) {
     var ySpeed = 0;
     var xAcc = 0;
     var yAcc = 1;
-    const max_speed = 10;
-
+    const max_speed = 20;
+    let butt;
 
     sketch.setup = function() {
         h = sketch.windowHeight;
         w = sketch.windowWidth;
-
-
         cnv = sketch.createCanvas(400,400 );
         xPos = sketch.width/2;
         yPos = sketch.height/4;
-        //cnv.parent("p5sketch")
+        //cnv.parent("p5sketch2")
         sketch.background(255, 0, 200);
     }
 
@@ -70,17 +94,24 @@ var s = function( sketch ) {
         sketch.rect(0,sketch.height-50,sketch.width,50)
         sketch.update()
     }
+    sketch.reset = function() {
+        xSpeed = 0;
+        ySpeed = 0;
+        xPos = sketch.width/2;
+        yPos = sketch.height/4;
 
+    }
     sketch.update = function() {
-        xPos += xSpeed;
-        yPos += ySpeed;
         xSpeed += xAcc;
         ySpeed += yAcc;
+        xPos += xSpeed;
+        yPos += ySpeed;
+
         if (xSpeed > max_speed) {
-            xSpeed = 10;
+            xSpeed = 20;
         }
         if (ySpeed > max_speed) {
-            ySpeed = 10;
+            ySpeed = 20;
         }
         
     }
@@ -153,4 +184,4 @@ var s = function( sketch ) {
         
     }
 };
-  var myp5_2 = new p5(s3, 'p5sketch3');
+  var myp5_3 = new p5(s3, 'p5sketch3');
